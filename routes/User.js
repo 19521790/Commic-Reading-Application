5400b715fd8a0584bb5b8eecde68726eba0d6333
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2023-08-01 18:10:55 +0700

    Fix typo

--- routes/User.js
+++ routes/User.js
@@ -4,48 +4,48 @@ const sqlConnection = require("./sqlConnection");
 
 //lấy toàn bộ thông tin user
 router.get("/", (req, res) => {
-    let sql = "select * from user";
-    sqlConnection(sql, (err, results) => {
-        if (err) {
-            throw err;
-        }
-        res.send(results);
-    });
+  let sql = "select * from user";
+  sqlConnection(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.send(results);
+  });
 });
 
 //check user
 router.get("/get/:email", (req, res) => {
-    let sql = "select * from user where Email=?";
-    sqlConnection(sql, [req.params.idUser], (err, results) => {
-        if (err) {
-            res.send(false);
-        } else {
-            if (results.length == 0) {
-                res.send(false);
-            } else {
-                res.send(results);
-            }
-        }
-    });
+  let sql = "select * from user where Email=?";
+  sqlConnection(sql, [req.params.email], (err, results) => {
+    if (err) {
+      res.send(false);
+    } else {
+      if (results.length == 0) {
+        res.send(false);
+      } else {
+        res.send(results);
+      }
+    }
+  });
 });
 
-
 router.post("/", (req, res) => {
-    const postinfo = {
-        idUser: req.body.UserId,
-        Name: req.body.UserName,
-        Email: req.body.UserEmail,
-        Image: req.body.UserImage,
-    };
+  const postinfo = {
+    idUser: req.body.UserId,
+    Name: req.body.UserName,
+    Email: req.body.UserEmail,
+    Image: req.body.UserImage,
+  };
 
-    let sql = "insert into user set ?";
-    sqlConnection(sql, [postinfo], (err, results) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("success");
-        }
-    });
+  let sql = "insert into user set ?";
+  sqlConnection(sql, [postinfo], (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+      res.send("success");
+    }
+  });
 });
 
 module.exports = router;
